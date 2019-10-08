8234ce3b80e514684718d7cca36bc0978698f2a3
roger-mahler <roger.mahler@hotmail.com>
2019-10-08 10:35:50 +0200

    TArget filename change

--- runner.py
+++ runner.py
@@ -6,5 +6,6 @@ stopwords = set(nltk.corpus.stopwords.words('swedish')).union({ "politisk", "pol
 options   = dict(to_lower=True, deacc=False, min_len=2, max_len=None, numerals=False, filter_stopwords=False, stopwords=stopwords)
 years = list(range(1945, 1990))
 source_filename = './westac/case_study_political_in_newspapers/data/year+text_window.txt'
-target_filename = 'co_occurrence_1945-1989.csv'
+target_filename = 'co_occurrence_{min(years)}-{max(years)}.csv'
+
 political.compute_co_ocurrence_for_year(source_filename, years, target_filename, min_count=3, **options)
