3cfdafc4d07539dc6849213f65b1e749796e5493
roger-mahler <roger.mahler@hotmail.com>
2019-10-08 09:48:59 +0200

    Changes min_count

--- runner.py
+++ runner.py
@@ -5,4 +5,6 @@ from westac.case_study_political_in_newspapers import window_coocurrence as poli
 stopwords = set(nltk.corpus.stopwords.words('swedish')).union({ "politisk", "politiska", "politiskt" })
 options   = dict(to_lower=True, deacc=False, min_len=2, max_len=None, numerals=False, filter_stopwords=False, stopwords=stopwords)
 years = list(range(1945, 1990))
-political.compute_co_ocurrence_for_year('./westac/case_study_political_in_newspapers/data/year+text_window.txt', years, 'co_occurrence_1945-1989.xlsx', options)
+source_filename = './westac/case_study_political_in_newspapers/data/year+text_window.txt'
+target_filename = 'co_occurrence_1945-1989.csv'
+political.compute_co_ocurrence_for_year(source_filename, years, target_filename, min_count=3, **options)
