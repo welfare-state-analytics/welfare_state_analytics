047dc1cb96d470b2f82c12f8b95b2d40680d5f5b
roger-mahler <roger.mahler@hotmail.com>
2019-10-08 11:23:14 +0200

    Changed to Excel output

--- runner.py
+++ runner.py
@@ -8,6 +8,6 @@ min_count = 3
 stopwords = set(nltk.corpus.stopwords.words('swedish')).union({ "politisk", "politiska", "politiskt" })
 options   = dict(to_lower=True, deacc=False, min_len=2, max_len=None, numerals=False, filter_stopwords=False, stopwords=stopwords)
 source_filename = './westac/case_study_political_in_newspapers/data/year+text_window.txt'
-target_filename = 'co_occurrence_{}-{}.csv'.format(min(years), max(years))
+target_filename = 'co_occurrence_{}-{}.xlsx'.format(min(years), max(years))
 
 political.compute_co_ocurrence_for_year(source_filename, years, target_filename, min_count=min_count, **options)
