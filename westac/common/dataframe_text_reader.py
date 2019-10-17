6d879b3a4c60a5b6824983218666c7a0df3a3fc0
roger-mahler <roger.mahler@hotmail.com>
2019-10-17 13:21:11 +0200

    Moved text readers to own files

---
+++ westac/common/dataframe_text_reader.py
@@ -0,0 +1,49 @@
+import types
+
+class DataFrameTextReader:
+
+    def __init__(self, df, **column_filters):
+
+        assert 'txt' in df.columns
+        assert 'year' in df.columns
+
+        self.df = df
+
+        if not 'filename' in self.df.columns:
+            self.df['filename'] = self.df.index.astype(str)
+
+        for column, value in column_filters.items():
+            assert column in self.df.columns, column + ' is missing'
+            self.df = self.df[self.df[column] == value]
+
+        if len(self.df[self.df.txt.isna()]) > 0:
+            print('Warn: {} n/a rows encountered'.format(len(self.df[self.df.txt.isna()])))
+            self.df = self.df.dropna()
+
+        self.iterator = None
+        self.metadata = self.compile_metadata()
+        self.metadict = { x.filename: x for x in (self.metadata or [])}
+        self.filenames = [ x.filename for x in self.metadata ]
+
+    def __iter__(self):
+
+        self.iterator = None
+        return self
+
+    def __next__(self):
+
+        if self.iterator is None:
+            self.iterator = self.get_iterator()
+
+        return next(self.iterator)
+
+    def get_iterator(self):
+        return ((row['filename'], row['txt']) for _, row in self.df.iterrows())
+
+    def compile_metadata(self):
+        assert 'filename' in self.df.columns
+        df_m = self.df[[ x for x in list(self.df.columns) if x != 'txt' ]]
+        metadata = [
+            types.SimpleNamespace(**meta) for meta in df_m.to_dict(orient='records')
+        ]
+        return metadata
---
+++ westac/common/file_text_reader.py
@@ -0,0 +1,53 @@
+import os
+
+from . utility import list_files, extract_metadata, read_file, dehyphen, compress_whitespaces
+
+class FileTextReader:
+
+    def __init__(self, path, pattern='*.txt', itemfilter=None, meta_extract=None, compress_whitespaces=True, dehyphen=True):
+        self.path = path
+        self.is_zip = os.path.isfile(path) # and path.endswith('zip')
+        self.filename_pattern = pattern
+        self.archive_filenames = list_files(path, pattern)
+        self.compress_whitespaces = compress_whitespaces
+        self.dehyphen = dehyphen
+        filenames = None
+        if itemfilter is not None:
+            if isinstance(itemfilter, list):
+                filenames = [ x for x in itemfilter if x in self.archive_filenames ]
+            elif callable(itemfilter):
+                filenames = [ x for x in self.archive_filenames if itemfilter(self.archive_filenames, x) ]
+            else:
+                assert False
+        self.filenames = filenames or self.archive_filenames
+        self.iterator = None
+        self.metadata = [ extract_metadata(x, **meta_extract) for x in self.filenames] if not meta_extract is None else None
+        self.metadict = { x.filename: x for x in (self.metadata or [])}
+
+    def __iter__(self):
+        self.iterator = None
+        return self
+
+    def __next__(self):
+        if self.iterator is None:
+            self.iterator = self.get_iterator()
+        return next(self.iterator)
+
+    def get_file(self, filename):
+
+        if filename not in self.filenames:
+            yield os.path.basename(filename), None
+
+        yield self.metadict.get(filename, filename), self.read_content(filename)
+
+    def get_iterator(self):
+        for filename in self.filenames:
+            yield os.path.basename(filename), self.read_content(filename)
+
+    def read_content(self, filename):
+        content = read_file(self.path, filename)
+        if self.dehyphen:
+            content = dehyphen(content)
+        if self.compress_whitespaces:
+            content = compress_whitespaces(content)
+        return content
