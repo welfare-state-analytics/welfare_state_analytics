0dc3e9080c5031b3820d7cdda06ce00c29f6db0b
roger-mahler <roger.mahler@hotmail.com>
2019-10-30 16:03:40 +0100

    New run parameters

--- co_ocurrence_runner.py
+++ co_ocurrence_runner.py
@@ -1,13 +1,20 @@
 
+import time
+
 from westac.case_study_political_in_newspapers import window_coocurrence as political
 from westac.case_study_political_in_newspapers.stopwords import stopwords
 
-years      = list(range(1945, 1990))
+#periods    = list(range(1945, 1990))
+periods    = [ (1945, 1954), (1955, 1964), (1965, 1974), (1975, 1984) ]
 newspapers = [ 'AFTONBLADET', 'EXPRESSEN', 'DAGENS NYHETER', 'SVENSKA DAGBLADET' ]
-min_count  = 3
+min_count  = 0
 
-options   = dict(to_lower=True, deacc=False, min_len=2, max_len=None, numerals=False, filter_stopwords=False, stopwords=stopwords)
-source_filename = './westac/case_study_political_in_newspapers/data/year+newspaper+text_2019-10-16.txt'
-target_filename = 'co_occurrence_{}-{}.csv'.format(min(years), max(years))
+options   = dict(to_lower=True, deacc=False, min_len=2, max_len=None, numerals=False, symbols=False, filter_stopwords=False, stopwords=stopwords)
+source_filename = './data/year+newspaper+text_2019-10-16.txt'
+target_filename = './output/political_co_occurrence_{}.csv'.format(time.strftime("%Y%m%d_%H%M%S"))
 
-political.compute_co_ocurrence_for_year(source_filename, newspapers, years, target_filename, min_count=min_count, **options)
+#political.compute_co_ocurrence_for_periods(source_filename, newspapers, periods, target_filename, min_count=min_count, **options)
+
+for year in [1948, 1958, 1968, 1978, 1988]:
+    target_filename = './output/political_co_occurrence_{}.csv.zip'.format(year)
+    political.compute_co_ocurrence_for_periods(source_filename, newspapers, [year], target_filename, min_count=min_count, **options)
