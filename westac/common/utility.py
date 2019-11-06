c6234c4bb6c3f6b2d2842aa19e222b3e2d6c24df
roger-mahler <roger.mahler@hotmail.com>
2019-11-06 20:55:21 +0100

    Improved logging

--- westac/common/utility.py
+++ westac/common/utility.py
@@ -5,8 +5,34 @@ import re
 import types
 import typing
 import os
+import logging
+import time
 
 HYPHEN_REGEXP = re.compile(r'\b(\w+)-\s*\r?\n\s*(\w+)\b', re.UNICODE)
+
+def setup_logger(logger=None, filename=None, level=logging.DEBUG):
+    '''
+    Setup logging of import messages to both file and console
+    '''
+    if logger is None:
+        logger = logging.getLogger("westac")
+
+    filename = filename or 'westac_{}.log'.format(time.strftime("%Y%m%d-%H%M%S"))
+    logger.handlers = []
+
+    logger.setLevel(level)
+    formatter = logging.Formatter('%(message)s')
+
+    fh = logging.FileHandler(filename)
+    fh.setLevel(level)
+    fh.setFormatter(formatter)
+    logger.addHandler(fh)
+
+    ch = logging.StreamHandler()
+    ch.setLevel(level)
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
+    return logger
 
 def flatten(l):
     return [ x for ws in l for x in ws]
