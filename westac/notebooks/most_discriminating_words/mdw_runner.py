90f156187d2f30830a2ad1ab8d6abcc0ef93878a
roger-mahler <roger.mahler@hotmail.com>
2020-02-17 15:50:16 +0100

    MDW runner script

---
+++ westac/notebooks/most_discriminating_words/mdw_runner.py
@@ -0,0 +1,37 @@
+import sys, os
+
+root_folder = "/Users/frno0044/Documents/Kod/welfare_state_analytics"
+corpus_folder = os.path.join(root_folder, "output")'
+sys.path = [ root_folder ] + sys.path
+
+import westac.corpus.vectorized_corpus as vectorized_corpus
+
+from westac.notebooks.most_discriminating_words.most_discriminating_terms_gui import display_gui
+from westac.notebooks.most_discriminating_words.most_discriminating_terms import compute_most_discriminating_terms
+
+v_corpus = vectorized_corpus.VectorizedCorpus\
+    .load('SOU_1945-1989_NN+VB+JJ_lemma_L0_+N_+S', corpus_folder)\
+    .slice_by_n_count(10)\
+    .slice_by_n_top(500000)
+
+df = compute_most_discriminating_terms(
+    v_corpus,
+    v_corpus.document_index,
+    top_n_terms=250,
+    max_n_terms=2000,
+    period1=(1945, 1967),
+    period2=(1968, 1989)
+)
+
+df.to_excel('sou_mdw_45-67_vs_68-89.xlsx')
+
+df = compute_most_discriminating_terms(
+    v_corpus,
+    v_corpus.document_index,
+    top_n_terms=250,
+    max_n_terms=2000,
+    period1=(1945, 1967),
+    period2=(1968, 1989)
+)
+
+df.to_excel('sou_mdw_45-67_vs_68-89.xlsx')
