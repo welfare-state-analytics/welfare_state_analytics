cdc297a97e1c66030474158dfb7d2b87c5af9575
roger-mahler <roger.mahler@hotmail.com>
2020-02-27 07:06:03 +0100

    Rollback of #32.

--- westac/notebooks/political_in_newspapers/corpus_data.py
+++ westac/notebooks/political_in_newspapers/corpus_data.py
@@ -152,7 +152,7 @@ def load_as_dtm(corpus_folder):
 
 def load_as_gensim_sparse_corpus(corpus_folder):
     v_dtm = load_as_sparse_matrix(corpus_folder)
-    g_corpus = Sparse2Corpus(v_dtm, documents_columns=True)
+    g_corpus = Sparse2Corpus(v_dtm, documents_columns=False)
     df_documents = load_documents(corpus_folder)
     df_vocabulary = load_vocabulary_file_as_data_frame(corpus_folder)
     id2token = df_vocabulary['token'].to_dict()
