3adbf2047e0e6ccd048929cd0d2ac9b7b0e9fccf
roger-mahler <roger.mahler@hotmail.com>
2020-03-03 20:28:24 +0100

    Changed options

--- run.sh
+++ run.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
 corpus_folder="./data/textblock_politisk"
+max_iter=2000
+n_workers=7
 
-run_task() {
+function run_task() {
+    engine=$1
+    n_topics=$2
     model_name="$1.topics.$2.AB.DN"
     target_folder="$corpus_folder/$model_name"
     mkdir -p $target_folder
-    python run_lda.py $model_name --n-topics $2 --engine "$1" --workers 7 --prefix "$target_folder/" > $target_folder/run.log 2>&1
+    python run_lda.py $model_name --n-topics $2 --engine "$1" --workers $n_workers --max-iter $max_iter --prefix "$target_folder/" > $target_folder/run.log 2>&1
     gzip $target_folder/*.txt
 }
 
 run_task "gensim_mallet-lda" 50
-run_task "gensim_mallet-lda" 100
-run_task "gensim_mallet-lda" 200
+# run_task "gensim_mallet-lda" 100
+# run_task "gensim_mallet-lda" 200
+# run_task "gensim_mallet-lda" 400
 
 
