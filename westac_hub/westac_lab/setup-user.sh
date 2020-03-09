399fa7c8f76878f67518cf43f60283550f14b21c
roger-mahler <roger.mahler@hotmail.com>
2020-03-09 09:21:02 +0100

    Refactored away shell scripts

--- westac_hub/westac_lab/setup-user.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-adduser $LAB_USER --uid $LAB_UID --gid $LAB_GID --disabled-password --gecos '' --shell /bin/bash
-
-adduser $LAB_USER sudo
-
-echo '%sudo ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
-
-mkdir -p $HOME/work
