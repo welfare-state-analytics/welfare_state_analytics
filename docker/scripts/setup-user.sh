17b0a651c95544a97aa4b2d773eedfbc2700481c
roger-mahler <roger.mahler@hotmail.com>
2020-06-05 13:11:26 +0200

    Removed deprecated docker files

--- docker/scripts/setup-user.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-adduser $PUSER --uid $PUID --gid $PGID --disabled-password --gecos '' --shell /bin/bash
-
-adduser $PUSER sudo
-
-echo '%sudo ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
-
-mkdir -p $HOME/work
