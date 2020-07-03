01a73ecdd57c25575fce47daef2e218711431c43
roger-mahler <roger.mahler@hotmail.com>
2020-07-03 15:28:07 +0200

    Moved function into package

--- westac/corpus/corpus_vectorizer.py
+++ westac/corpus/corpus_vectorizer.py
@@ -1,10 +1,15 @@
-
+import os
 import pandas as pd
-import numpy as np
-import sklearn.preprocessing
 
 from sklearn.feature_extraction.text import CountVectorizer
+
+from westac.corpus import text_corpus
 from westac.corpus import vectorized_corpus
+from westac.corpus import file_text_reader
+
+import logging
+
+logger = logging.getLogger("corpus_vectorizer")
 
 class CorpusVectorizer():
 
@@ -43,3 +48,36 @@ class CorpusVectorizer():
         df['document_id'] = list(df.index)
         return df
 
+def generate_corpus(filename, output_folder, **kwargs):
+
+    if not os.path.isfile(filename):
+        logger.error('no such file: {}'.format(filename))
+        return
+
+    dump_tag = '{}_{}_{}_{}'.format(
+        os.path.basename(filename).split('.')[0],
+        'L{}'.format(kwargs.get('min_len', 0)),
+        '-N' if kwargs.get('numerals', False) else '+N',
+        '-S' if kwargs.get('symbols', False) else '+S',
+    )
+
+    if vectorized_corpus.VectorizedCorpus.dump_exists(dump_tag):
+        logger.info('removing existing result files...')
+        os.remove(os.path.join(output_folder, '{}_vector_data.npy'.format(dump_tag)))
+        os.remove(os.path.join(output_folder, '{}_vectorizer_data.pickle'.format(dump_tag)))
+
+    logger.info('Creating new corpus...')
+    reader = file_text_reader.FileTextReader(
+        filename, meta_extract=kwargs.get("meta_extract"),
+        compress_whitespaces=True,
+        dehyphen=True,
+        pattern=kwargs.get("pattern", "*.txt")
+    )
+    corpus = text_corpus.ProcessedCorpus(reader, **kwargs)
+
+    logger.info('Creating document-term matrix...')
+    vectorizer = CorpusVectorizer()
+    v_corpus = vectorizer.fit_transform(corpus)
+
+    logger.info('Saving data matrix...')
+    v_corpus.dump(tag=dump_tag, folder=output_folder)
