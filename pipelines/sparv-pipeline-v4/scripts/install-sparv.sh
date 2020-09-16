94bf9b90ff976cccd88ec0ac8ebaace16c116334
Roger Mähler <roger.mahler@hotmail.com>
2020-09-16 09:45:30 +0200

    Fixed install of models

--- pipelines/sparv-pipeline-v4/scripts/install-sparv.sh
+++ pipelines/sparv-pipeline-v4/scripts/install-sparv.sh
@@ -15,9 +15,9 @@ if [ $BUILD_SPARV_MODELS == "yes" ]; then
     sparv build-models --language swe
 elif [ -d /sparv/models ]; then
     echo "info: models found in /sparv/models (no build will be started)"
-    sparv build-models --language swe
 else
     echo "info: models not found or expected to be mounted /sparv/models"
+    mkdir -p /sparv/models
 fi
 
 sparv setup << EOF > /dev/null
