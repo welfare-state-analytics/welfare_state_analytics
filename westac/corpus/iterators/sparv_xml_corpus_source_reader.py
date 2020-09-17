9b46a165db0b9637d91a4ec5f46beaaf9eef2620
Roger Mähler <roger.mahler@hotmail.com>
2020-09-17 08:57:32 +0200

    Added versin argument

--- westac/corpus/iterators/sparv_xml_corpus_source_reader.py
+++ westac/corpus/iterators/sparv_xml_corpus_source_reader.py
@@ -9,11 +9,21 @@ from .corpus_source_reader import CorpusSourceReader
 
 logger = logging.getLogger(__name__)
 
+DEFAULT_OPTS = dict(
+    postags='',
+    lemmatize=True,
+    chunk_size=None,
+    xslt_filename=None,
+    delimiter="|",
+    append_pos="",
+    ignores="|MAD|MID|PAD|"
+)
+
 class SparvXmlCorpusSourceReader(CorpusSourceReader):
 
-    def __init__(self, source, transforms=None, postags=None, lemmatize=True, chunk_size=None, xslt_filename=None, deliminator="|", append_pos="", ignores="|MAD|MID|PAD|"):
+    def __init__(self, source, transforms=None, postags=None, lemmatize=True, chunk_size=None, xslt_filename=None, delimiter="|", append_pos="", ignores="|MAD|MID|PAD|", version=4):
 
-        tokenize = lambda x: str(x).split(deliminator)
+        tokenize = lambda x: str(x).split(delimiter)
 
         super(SparvXmlCorpusSourceReader, self).__init__(source, transforms, chunk_size, pattern='*.xml', tokenize=tokenize, as_binary=True)
 
@@ -21,7 +31,7 @@ class SparvXmlCorpusSourceReader(CorpusSourceReader):
         self.lemmatize = lemmatize
         self.append_pos = append_pos
         self.ignores = ignores
-
+        self.xslt_filename = XSLT_FILENAME_V3 if version == 3 else xslt_filename
         self.parser = SparvXml2Text(xslt_filename=xslt_filename, postags=postags, lemmatize=lemmatize, append_pos=append_pos, ignores=ignores)
 
     def preprocess(self, content):
@@ -30,7 +40,7 @@ class SparvXmlCorpusSourceReader(CorpusSourceReader):
 
 class Sparv3XmlCorpusSourceReader(SparvXmlCorpusSourceReader):
 
-    def __init__(self, source, transforms=None, postags=None, lemmatize=True, chunk_size=None, deliminator="|", append_pos="", ignores="|MAD|MID|PAD|"):
+    def __init__(self, source, transforms=None, postags=None, lemmatize=True, chunk_size=None, delimiter="|", append_pos="", ignores="|MAD|MID|PAD|"):
 
         super(Sparv3XmlCorpusSourceReader, self).__init__(
             source,
@@ -39,7 +49,7 @@ class Sparv3XmlCorpusSourceReader(SparvXmlCorpusSourceReader):
             lemmatize=lemmatize,
             chunk_size=chunk_size,
             xslt_filename=XSLT_FILENAME_V3,
-            deliminator=deliminator,
+            delimiter=delimiter,
             append_pos=append_pos,
             ignores=ignores
         )
