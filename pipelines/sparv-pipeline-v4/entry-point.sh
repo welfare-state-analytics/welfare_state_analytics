26966b20864b6dae5ed934aafc671de6a2294005
roger-mahler <roger.mahler@hotmail.com>
2020-09-25 08:22:09 +0200

    Moves to westac_data

--- pipelines/sparv-pipeline-v4/entry-point.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export PATH="/home/.local/bin:$PATH"
-
-cd /work
-
-whoami
-
-/home/.local/bin/sparv $@
--- pipelines/sparv-pipeline-v4/scripts/install-sparv-wsd.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Sparv-wsd
-
-mkdir -p $SPARV_DIR/bin/wsd
-cd $SPARV_DIR/bin/wsd
-wget https://github.com/spraakbanken/sparv-wsd/raw/master/bin/saldowsd.jar
