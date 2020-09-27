000e49416d05a10ffa96c6249b8fe51545909baa
roger-mahler <roger.mahler@hotmail.com>
2020-09-27 12:38:37 +0200

    Sorted script files

---
+++ scripts/install/install_extensions.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+jupyter labextension install \
+    @jupyter-widgets/jupyterlab-manager \
+    beakerx-jupyterlab \
+    @bokeh/jupyter_bokeh \
+    jupyter-matplotlib \
+    jupyterlab-jupytext \
+    ipyaggrid
