201d3bfb291f6e949bcbf4333450f401fc90212d
roger-mahler <roger.mahler@hotmail.com>
2020-09-29 09:52:42 +0200

    More descriptive exception message

--- westac/common/file_utility.py
+++ westac/common/file_utility.py
@@ -100,7 +100,7 @@ def list_filenames(folder_or_zip: Union[str, zipfile.ZipFile], filename_pattern:
 
     if filenames is None:
 
-        raise ValueError("Source not found. Only folder or ZIP or file are valid arguments")
+        raise ValueError(f"Source '{folder_or_zip}' not found. Only folder or ZIP or file are valid arguments")
 
     return [
         filename for filename in sorted(filenames)
