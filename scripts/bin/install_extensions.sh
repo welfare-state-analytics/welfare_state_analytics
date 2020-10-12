dcfb5bdd6b25369fd1d78b98d724babfe61b29f5
Roger Mähler <roger.mahler@hotmail.com>
2020-10-12 18:55:00 +0200

    Updated script

--- scripts/bin/install_extensions.sh
+++ scripts/bin/install_extensions.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
 
-pipenv run jupyter labextension install \
+poetry run jupyter labextension install \
     @jupyter-widgets/jupyterlab-manager \
-    beakerx-jupyterlab \
     @bokeh/jupyter_bokeh \
     jupyter-matplotlib \
     jupyterlab-jupytext \
     ipyaggrid
 
-pipenv run jupyter labextension install \
-    @jupyter-widgets/jupyterlab-manager \
-    @bokeh/jupyter_bokeh \
-    jupyter-matplotlib \
-    jupyterlab-jupytext \
-    ipyaggrid
