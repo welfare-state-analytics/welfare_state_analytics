b290f0314febcbf7e375375eda444877ac0424fd
Roger Mähler <roger.mahler@hotmail.com>
2020-10-12 10:19:37 +0200

    Removed dups

--- notebooks/common/utils.py
+++ notebooks/common/utils.py
@@ -1,4 +1,7 @@
+from typing import Dict, List, Tuple
+
 import pandas as pd
+from penelope.utility import flatten
 
 
 def setup_pandas():
@@ -9,9 +12,5 @@ def setup_pandas():
     pd.set_option('max_colwidth', 300)
 
 
-def flatten(lst):
-    return [x for ws in lst for x in ws]
-
-
-def to_text(document, id2token):
+def to_text(document: List[Tuple[int, int]], id2token: Dict[int, str]):
     return ' '.join(flatten([f * [id2token[token_id]] for token_id, f in document]))
