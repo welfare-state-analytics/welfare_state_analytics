3e8e491da8488daac014794e6aaa460b86641cf0
Roger Mähler <roger.mahler@hotmail.com>
2020-10-14 18:14:16 +0200

    Update module exports

--- notebooks/common/__init__.py
+++ notebooks/common/__init__.py
@@ -1,3 +1,4 @@
 from .model_container import TopicModelContainer
-from .topic_utility import *
+from .topic_utility import filter_by_key_value, filter_document_topic_weights
 from .utils import setup_pandas, to_text
+from .ipyaggrid_utility import display_grid
