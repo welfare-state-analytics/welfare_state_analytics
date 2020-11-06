1dccda8198af0abc204ae797277ea40eecc5ad85
Roger Mähler <roger.mahler@hotmail.com>
2020-11-06 15:23:21 +0100

    Added install scripts

---
+++ notebooks/bin/install.bat
@@ -0,0 +1,23 @@
+#@echo off
+
+set nltk_data_folder="%HOME%\nltk_data
+
+iF "%NLTK_DATA%"=="" (
+    not not exists "%nltk_data_folder% (
+        mkdir "%HOME%\nltk_data"
+    )
+    "Setting NLTK_DATA to %HOME%\nltk_data"
+    echo mkdir "%HOME%\nltk_data"
+    echo setx NLTK_DATA="$HOME/nltk_data"
+)
+
+poetry run python -m nltk.downloader -d $(NLTK_DATA) stopwords punkt sentiwordnet
+
+poetry run jupyter labextension install \
+    @jupyter-widgets/jupyterlab-manager \
+    @bokeh/jupyter_bokeh \
+    jupyter-matplotlib \
+    jupyterlab-jupytext \
+    ipyaggrid \
+    qgrid2
+
