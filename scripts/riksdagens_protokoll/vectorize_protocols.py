f97f13c2c6a62a665e949154726e54e56361f1e6
Roger Mähler <roger.mahler@hotmail.com>
2020-11-06 15:28:03 +0100

    Adapted to penelope API

--- scripts/riksdagens_protokoll/vectorize_protocols.py
+++ scripts/riksdagens_protokoll/vectorize_protocols.py
@@ -1,20 +1,26 @@
-import penelope.corpus.vectorizer as corpus_vectorizer
+from penelope.corpus.tokens_transformer import TokensTransformOpts
+from penelope.workflows import vectorize_tokenized_corpus_workflow
 
-kwargs = dict(
-    only_any_alphanumeric=True,
-    to_lower=True,
-    remove_accents=False,
-    min_len=2,
-    max_len=None,
+tokens_transform_opts = TokensTransformOpts(
     keep_numerals=False,
     keep_symbols=False,
+    max_len=None,
+    min_len=2,
     only_alphabetic=True,
-    pattern='*.txt',
-    filename_fields={'year': r"prot\_(\d{4}).*", 'year2': r"prot_\d{4}(\d{2})__*", 'number': r"prot_\d+__(\d+).*"},
+    only_any_alphanumeric=True,
+    remove_accents=False,
+    to_lower=True,
 )
-
 
 corpus_filename = './data/riksdagens_protokoll/riksdagens_protokoll_content_corpus_1945-1989.zip'
 output_folder = './data/riksdagens_protokoll/'
 
-corpus_vectorizer.generate_corpus(corpus_filename, output_folder=output_folder, **kwargs)
+vectorize_tokenized_corpus_workflow(
+    input_filename=corpus_filename,
+    output_folder=output_folder,
+    output_tag="abcdefg",
+    filename_field={'year': r"prot\_(\d{4}).*", 'year2': r"prot_\d{4}(\d{2})__*", 'number': r"prot_\d+__(\d+).*"},
+    filename_pattern='*.txt',
+    count_threshold=1,
+    tokens_transform_opts=tokens_transform_opts,
+)
