afeef4e70bc0083e7d358319f00c3cc214972431
Roger Mähler <roger.mahler@hotmail.com>
2020-11-06 15:24:11 +0100

    Deprecated file

---
+++ scripts/bin/install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ "$NLTK_DATA" == "" ]; then
+    mkdir -p $HOME/nltk_data
+    export NLTK_DATA=$HOME/nltk_data
+fi
+
+poetry run python -m nltk.downloader -d $(NLTK_DATA) stopwords punkt sentiwordnet
+
+poetry run jupyter labextension install \
+    @jupyter-widgets/jupyterlab-manager \
+    @bokeh/jupyter_bokeh \
+    jupyter-matplotlib \
+    jupyterlab-jupytext \
+    ipyaggrid \
+    qgrid2
+
--- scripts/bin/install_extensions.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-poetry run jupyter labextension install \
-    @jupyter-widgets/jupyterlab-manager \
-    @bokeh/jupyter_bokeh \
-    jupyter-matplotlib \
-    jupyterlab-jupytext \
-    ipyaggrid
-
