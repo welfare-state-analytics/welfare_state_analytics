8b94cf50e36c479bda3bfbaf0f8216687f49dcce
Roger Mähler <roger.mahler@hotmail.com>
2020-11-10 14:48:27 +0100

    Added word trend tests

---
+++ tests/word_trends_test.py
@@ -0,0 +1,37 @@
+
+import pandas as pd
+from notebooks.word_trends.gui_callback import loaded_callback, State, build_layout
+from penelope.notebook.utility import OutputsTabExt
+
+def xtest_loaded_callback():
+    pass
+
+def test_build_layout():
+
+    # FIXME: Use mocks/patches!
+    state = State(
+        compute_options={},
+        corpus=None,
+        corpus_folder="",
+        corpus_tag="",
+        goodness_of_fit=pd.DataFrame(data={k: [] for k in             [
+                "token",
+                "word_count",
+                "l2_norm",
+                "slope",
+                "chi2_stats",
+                "earth_mover",
+                "kld",
+                "skew",
+                "entropy",
+            ]}),
+        most_deviating=pd.DataFrame(data={'l2_norm_token':[], 'l2_norm':[], 'abs_l2_norm':[]}),
+        most_deviating_overview=pd.DataFrame(data={'l2_norm_token':[], 'l2_norm':[], 'abs_l2_norm':[]})
+    )
+
+    w: OutputsTabExt = build_layout(state=state)
+
+    assert w is not None and isinstance(w, OutputsTabExt)
+    assert 2 == len(w.children)
+    #assert 4 == len(w.children[1].children)
+
