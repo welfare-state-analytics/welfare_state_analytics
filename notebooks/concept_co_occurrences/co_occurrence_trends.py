1085a2f75ab849c0a56838eafe5cdc48a396d2e9
Roger Mähler <roger.mahler@hotmail.com>
2020-11-11 11:43:39 +0100

    Removed  unrelated GUI

--- notebooks/concept_co_occurrences/co_occurrence_trends.py
+++ notebooks/concept_co_occurrences/co_occurrence_trends.py
@@ -17,16 +17,21 @@
 # %% [markdown]
 # ## Concept Context Co-Occurrences Analysis
 
-# %% tags=[]
+# %% [markdown]
+# ### Generate a new concept context co-co_occurrence
+# For long running tasks, please use the CLI `concept_co_occurrence` instead.
+# This function computes new concept context co-occurrence data and stores the result in a CSV file.
+# Optionally, the co-occurrence data can be transformed to a vectorized corpus to enable word trend exploration.
+# %%
 # %load_ext autoreload
 # %autoreload 2
-# pylint: disable=too-many-instance-attributes
+# pylint: disable=too-many-instance-attributes, unused-argument
 
+import importlib
 import warnings
 
 import penelope.notebook.concept_co_occurrences_gui as compute_gui
 import penelope.notebook.load_co_occurrences_gui as load_gui
-import penelope.notebook.load_vectorized_corpus_gui as load_vectorized_corpus_gui
 from bokeh.plotting import output_notebook
 
 import __paths__  # pylint: disable=unused-import
@@ -36,24 +41,13 @@ warnings.filterwarnings("ignore", category=FutureWarning)
 
 output_notebook()
 
-
-# %% [markdown]
-# ## Generate a new concept context co-co_occurrence
-# For long running tasks, please use the CLI `concept_co_occurrence` instead.
-# This function computes new concept context co-occurrence data and stores the result in a CSV file.
-# Optionally, the co-occurrence data can be transformed to a vectorized corpus to enable word trend exploration.
 # %%
-compute_gui.display_gui(data_folder=None, corpus_pattern='*sparv4.csv.zip', generated_callback=None)
+importlib.reload(compute_gui)
+compute_gui.display_gui(data_folder=None, corpus_pattern='*sparv4.csv.zip', generated_callback=loaded_callback)
 
 # %% [markdown]
-# ## Display concept context co-occurrences
+# ### Load saved concept context co-occurrences
 # %%
 load_gui.display_gui(
     data_folder='/home/roger', filename_suffix="coo_concept_context.csv.zip", loaded_callback=loaded_callback
 )
-
-# %% [markdown]
-# ### Load vectorized corpus and compute deviation metrics
-# Deviation metrics compares co-occurrences distribution to a uniform distribution
-# %%
-load_vectorized_corpus_gui.display_gui(loaded_callback=loaded_callback)
