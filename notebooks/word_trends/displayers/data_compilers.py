fbce206a6c6ae5303a9fc7e8222f864e2b0b7e3b
Roger Mähler <roger.mahler@hotmail.com>
2020-11-12 16:44:31 +0100

    Fixes Error when occurs when compiling multiline data #107 Added Bug Check for call fo function that requires corpus to be grouped by year.

--- notebooks/word_trends/displayers/data_compilers.py
+++ notebooks/word_trends/displayers/data_compilers.py
@@ -1,44 +1,48 @@
 import itertools
+from typing import Callable, Dict, List, Sequence
 
 import bokeh
+from penelope.corpus.vectorized_corpus import VectorizedCorpus
+from penelope.utility import take
 
 
-def take(n, iterable):
-    "Return first n items of the iterable as a list"
-    return list(itertools.islice(iterable, n))
+class PenelopeBugCheck(Exception):
+    pass
 
 
-def compile_multiline_data(x_corpus, indices, smoothers=None):
-
-    xs = x_corpus.xs_years()
-
+def compile_multiline_data(corpus: VectorizedCorpus, indices: List[int], smoothers: List[Callable] = None) -> Dict:
+    """Compile multiline plot data for token ids `indicies`, optionally applying `smoothers` functions"""
+    xs = corpus.xs_years()
+    # FIXME #107 Error when occurs when compiling multiline data
     if len(smoothers or []) > 0:
         xs_data = []
         ys_data = []
         for j in indices:
             xs_j = xs
-            ys_j = x_corpus.bag_term_matrix[:, j]
+            ys_j = corpus.bag_term_matrix[:, j]
             for smoother in smoothers:
                 xs_j, ys_j = smoother(xs_j, ys_j)
             xs_data.append(xs_j)
             ys_data.append(ys_j)
     else:
-        xs_data = [xs.tolist()] * len(indices)
-        ys_data = [x_corpus.bag_term_matrix[:, token_id].tolist() for token_id in indices]
+        xs_data = [[x for x in xs]] * len(indices)
+        ys_data = corpus.bag_term_matrix[:, indices].T.toarray().tolist()
 
     data = {
         'xs': xs_data,
         'ys': ys_data,
-        'label': [x_corpus.id2token[token_id].upper() for token_id in indices],
+        'label': [corpus.id2token[token_id].upper() for token_id in indices],
         'color': take(len(indices), itertools.cycle(bokeh.palettes.Category10[10])),
     }
     return data
 
 
-def compile_year_token_vector_data(x_corpus, indices, *args):  # pylint: disable=unused-argument
-
-    xs = x_corpus.xs_years()
-    data = {x_corpus.id2token[token_id]: x_corpus.bag_term_matrix[:, token_id] for token_id in indices}
+def compile_year_token_vector_data(corpus: VectorizedCorpus, indices: Sequence[int], *_) -> Dict:
+    """Extracts token's vectors for tokens ´indices` and returns a dict keyed by token"""
+    # FIXME: Logic assumes that vector is grouped_by_year
+    xs = corpus.xs_years()
+    if len(xs) != corpus.data.shape[0]:
+        raise PenelopeBugCheck(f"DTM shape{corpus.data.shape} is not compatible with year range {corpus.year_range()}")
+    data = {corpus.id2token[token_id]: corpus.bag_term_matrix[:, token_id] for token_id in indices}
     data['year'] = xs
-
     return data
