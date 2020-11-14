fa887354ff8622f0def57950b64d59075c53a9e5
Roger Mähler <roger.mahler@hotmail.com>
2020-11-14 23:32:16 +0100

    Removed test code

--- notebooks/word_trends/word_trends.py
+++ notebooks/word_trends/word_trends.py
@@ -26,48 +26,17 @@
 
 # %%
 
-import importlib
-
-import penelope.notebook.load_vectorized_corpus_gui as load_corpus_gui
-import penelope.notebook.utility as notebook_utility
 import penelope.notebook.vectorize_corpus_gui as vectorize_corpus_gui
+import penelope.notebook.vectorized_corpus_load_gui as load_corpus_gui
 from bokeh.plotting import output_notebook
 from penelope.notebook.word_trends.loaded_callback import loaded_callback
 
-output_notebook(hide_banner=True)
+output_notebook()
 
 # %% tags=[] vscode={"end_execution_time": "2020-08-31T18:34:55.995Z", "start_execution_time": "2020-08-31T18:34:55.854Z"}
-importlib.reload(vectorize_corpus_gui)
-importlib.reload(notebook_utility)
-importlib.reload(load_corpus_gui)
-importlib.reload(notebook_utility)
 
-# %% tags=[] vscode={"end_execution_time": "2020-08-31T18:34:55.995Z", "start_execution_time": "2020-08-31T18:34:55.854Z"}
-importlib.reload(vectorize_corpus_gui)
-
-data = None
-
-
-def loaded_callback_wrapper(
-    output,
-    *,
-    corpus=None,
-    corpus_folder: str = None,
-    corpus_tag: str = None,
-    n_count: int = 10000,
-    **kwargs,
-):
-    global data
-    data = locals()
-    loaded_callback(
-        output, corpus=corpus, corpus_folder=corpus_folder, corpus_tag=corpus_tag, n_count=n_count, **kwargs
-    )
-
-
-vectorize_corpus_gui.display_gui('*sparv4.csv.zip', generated_callback=loaded_callback_wrapper)
+vectorize_corpus_gui.display_gui('*sparv4.csv.zip', generated_callback=loaded_callback)
 
 
 # %%
 load_corpus_gui.display_gui(loaded_callback=loaded_callback)
-
-# %%
