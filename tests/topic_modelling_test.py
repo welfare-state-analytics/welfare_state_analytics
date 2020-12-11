2ea67524b7bd1b59a5409b558a768a7a8aca00b3
Roger Mähler <roger.mahler@hotmail.com>
2020-12-11 23:47:32 +0100

    Added mallet test

--- tests/topic_modelling_test.py
+++ tests/topic_modelling_test.py
@@ -1,27 +1,51 @@
 import os
 import shutil
 import uuid
+from typing import Any, Mapping
 
 import gensim
 import pandas as pd
 import penelope.topic_modelling as topic_modelling
+import pytest
 from penelope.corpus.text_lines_corpus import SimpleTextLinesCorpus
 from penelope.scripts.compute_topic_model import run_model
-from penelope.topic_modelling.container import InferredTopicsData, TrainingCorpus
+from penelope.topic_modelling.container import InferredModel, InferredTopicsData, TrainingCorpus
 
 jj = os.path.join
 
-TOPIC_MODELING_OPTS = {
-    'n_topics': 4,
-    'passes': 1,
-    'random_seed': 42,
-    'alpha': 'auto',
-    'workers': 1,
-    'max_iter': 100,
-    'prefix': '',
-}
-
 OUTPUT_FOLDER = "./tests/output/"
+
+TOPIC_MODELING_OPTS = {
+    "gensim_lda": {
+        'method': "gensim_lda",
+        'skip': False,
+        'class': gensim.models.ldamodel.LdaModel,
+        'run_opts': {
+            'n_topics': 4,
+            'passes': 1,
+            'random_seed': 42,
+            'alpha': 'auto',
+            'workers': 1,
+            'max_iter': 100,
+            'prefix': '',
+        },
+    },
+    "gensim_mallet-lda": {
+        'method': "gensim_mallet-lda",
+        'skip': os.environ.get('MALLET_HOME', None) is not None,
+        'class': gensim.models.wrappers.LdaMallet,
+        'run_opts': {
+            'default_mallet_home': os.environ.get('MALLET_HOME', None),
+            'n_topics': 4,  # note: mallet num_topics
+            'max_iter': 3000,  # note: mallet iterations
+            'prefix': OUTPUT_FOLDER,
+            'workers': 1,
+            'optimize_interval': 10,
+            'topic_threshold': 0.0,
+            'random_seed': 0,
+        },
+    },
+}
 
 
 class TranströmerCorpus(SimpleTextLinesCorpus):
@@ -33,9 +57,7 @@ class TranströmerCorpus(SimpleTextLinesCorpus):
         )
 
 
-def compute_inferred_model():
-
-    engine = "gensim_lda"
+def compute_inferred_model(engine: str, opts: Mapping[str, Any]) -> InferredModel:
 
     corpus: TranströmerCorpus = TranströmerCorpus()
     train_corpus = TrainingCorpus(
@@ -46,7 +68,7 @@ def compute_inferred_model():
     inferred_model = topic_modelling.infer_model(
         train_corpus=train_corpus,
         method=engine,
-        engine_args=TOPIC_MODELING_OPTS,
+        engine_args=opts,
     )
     return inferred_model
 
@@ -59,18 +81,18 @@ def test_tranströmers_corpus():
         assert len(tokens) > 0
 
 
-def test_infer_model():
+@pytest.mark.parametrize("opts", list(TOPIC_MODELING_OPTS.values()))
+def test_infer_model(opts):
 
-    inferred_model = compute_inferred_model()
+    inferred_model = compute_inferred_model(opts['method'], opts['run_opts'])
 
     assert inferred_model is not None
-    assert inferred_model.method == "gensim_lda"
-    assert isinstance(inferred_model.topic_model, gensim.models.ldamodel.LdaModel)
-    assert inferred_model.options["engine_options"] == TOPIC_MODELING_OPTS
+    assert inferred_model.method == opts['method']
+    assert isinstance(inferred_model.topic_model, opts['class'])
     assert isinstance(inferred_model.train_corpus.document_index, pd.DataFrame)
     assert len(inferred_model.train_corpus.corpus) == len(inferred_model.train_corpus.document_index)
     assert len(inferred_model.train_corpus.document_index) == 5
-    assert len(inferred_model.train_corpus.document_index.columns) == 6
+    assert len(inferred_model.train_corpus.document_index.columns) == 7
     assert 'n_terms' in inferred_model.train_corpus.document_index.columns
     assert inferred_model.train_corpus.corpus is not None
 
@@ -80,12 +102,13 @@ def test_infer_model():
     # cols = np.empty(n_nonzero, dtype=np.intc)
 
 
-def test_store_inferred_model():
+@pytest.mark.parametrize("opts", list(TOPIC_MODELING_OPTS.values()))
+def test_store_inferred_model(opts):
 
     # Arrange
     name = f"{uuid.uuid1()}"
     target_folder = os.path.join(OUTPUT_FOLDER, name)
-    inferred_model = compute_inferred_model()
+    inferred_model = compute_inferred_model(opts['method'], opts['run_opts'])
 
     # Act
     topic_modelling.store_model(inferred_model, target_folder)
@@ -96,12 +119,13 @@ def test_store_inferred_model():
     shutil.rmtree(target_folder)
 
 
-def test_store_gensim_model():
+@pytest.mark.parametrize("opts", list(TOPIC_MODELING_OPTS.values()))
+def test_store_gensim_model(opts):
 
     # Arrange
     folder = f"{uuid.uuid1()}"
     target_name = os.path.join(OUTPUT_FOLDER, folder, "test_corpus.gensim")
-    inferred_model = compute_inferred_model()
+    inferred_model = compute_inferred_model(opts['method'], opts['run_opts'])
     os.makedirs(os.path.join(OUTPUT_FOLDER, folder), exist_ok=True)
     # Act
     inferred_model.topic_model.save(target_name)
@@ -110,12 +134,12 @@ def test_store_gensim_model():
     assert os.path.isfile(target_name)
 
 
-def test_load_inferred_model_when_stored_corpus_is_true_has_same_loaded_trained_corpus():
+@pytest.mark.parametrize("opts", list(TOPIC_MODELING_OPTS.values()))
+def test_load_inferred_model_when_stored_corpus_is_true_has_same_loaded_trained_corpus(opts):
 
-    # Arrange
     name = f"{uuid.uuid1()}"
     target_folder = os.path.join(OUTPUT_FOLDER, name)
-    test_inferred_model = compute_inferred_model()
+    test_inferred_model = compute_inferred_model(opts['method'], opts['run_opts'])
     topic_modelling.store_model(test_inferred_model, target_folder, store_corpus=True)
 
     # Act
@@ -124,45 +148,45 @@ def test_load_inferred_model_when_stored_corpus_is_true_has_same_loaded_trained_
 
     # Assert
     assert inferred_model is not None
-    assert inferred_model.method == "gensim_lda"
-    assert isinstance(inferred_model.topic_model, gensim.models.ldamodel.LdaModel)
-    assert inferred_model.options["engine_options"] == TOPIC_MODELING_OPTS
+    assert inferred_model.method == opts['method']
+    assert isinstance(inferred_model.topic_model, opts['class'])
     assert isinstance(inferred_model.train_corpus.document_index, pd.DataFrame)
     assert len(inferred_model.train_corpus.corpus) == len(inferred_model.train_corpus.document_index)
     assert len(inferred_model.train_corpus.document_index) == 5
-    assert len(inferred_model.train_corpus.document_index.columns) == 6
+    assert len(inferred_model.train_corpus.document_index.columns) == 7
     assert 'n_terms' in inferred_model.train_corpus.document_index.columns
     assert inferred_model.train_corpus.corpus is not None
 
     shutil.rmtree(target_folder)
 
 
-def test_load_inferred_model_when_stored_corpus_is_false_has_no_trained_corpus():
+@pytest.mark.parametrize("opts", list(TOPIC_MODELING_OPTS.values()))
+def test_load_inferred_model_when_stored_corpus_is_false_has_no_trained_corpus(opts):
 
     # Arrange
     name = f"{uuid.uuid1()}"
     target_folder = os.path.join(OUTPUT_FOLDER, name)
-    test_inferred_model = compute_inferred_model()
+    test_inferred_model = compute_inferred_model(opts['method'], opts['run_opts'])
     topic_modelling.store_model(test_inferred_model, target_folder, store_corpus=False)
 
-    # Act
+    # Actopts['method'],
 
     inferred_model = topic_modelling.load_model(target_folder)
 
     # Assert
     assert inferred_model is not None
-    assert inferred_model.method == "gensim_lda"
-    assert isinstance(inferred_model.topic_model, gensim.models.ldamodel.LdaModel)
-    assert inferred_model.options["engine_options"] == TOPIC_MODELING_OPTS
+    assert inferred_model.method == opts['method']
+    assert isinstance(inferred_model.topic_model, opts['class'])
     assert inferred_model.train_corpus is None
 
     shutil.rmtree(target_folder)
 
 
-def test_infer_topics_data():
+@pytest.mark.parametrize("opts", list(TOPIC_MODELING_OPTS.values()))
+def test_infer_topics_data(opts):
 
     # Arrange
-    inferred_model = compute_inferred_model()
+    inferred_model = compute_inferred_model(opts['method'], opts['run_opts'])
 
     # Act
 
@@ -182,17 +206,18 @@ def test_infer_topics_data():
     assert isinstance(inferred_topics_data.topic_token_overview, pd.DataFrame)
     assert isinstance(inferred_topics_data.document_topic_weights, pd.DataFrame)
     assert inferred_topics_data.year_period == (2019, 2020)
-    assert inferred_topics_data.topic_ids == [0, 1, 2, 3]
+    assert set(inferred_topics_data.topic_ids) == {0, 1, 2, 3}
     assert len(inferred_topics_data.document_index) == 5
-    assert list(inferred_topics_data.topic_token_weights.topic_id.unique()) == [0, 1, 2, 3]
-    assert list(inferred_topics_data.topic_token_overview.index) == [0, 1, 2, 3]
-    assert list(inferred_topics_data.document_topic_weights.topic_id.unique()) == [0, 1, 2, 3]
+    assert set(inferred_topics_data.topic_token_weights.topic_id.unique()) == {0, 1, 2, 3}
+    assert set(inferred_topics_data.topic_token_overview.index) == {0, 1, 2, 3}
+    assert set(inferred_topics_data.document_topic_weights.topic_id.unique()) == {0, 1, 2, 3}
 
 
-def test_store_inferred_topics_data():
+@pytest.mark.parametrize("opts", list(TOPIC_MODELING_OPTS.values()))
+def test_store_inferred_topics_data(opts):
 
     # Arrange
-    inferred_model = compute_inferred_model()
+    inferred_model = compute_inferred_model(opts['method'], opts['run_opts'])
 
     inferred_topics_data: InferredTopicsData = topic_modelling.compile_inferred_topics_data(
         topic_model=inferred_model.topic_model,
@@ -216,10 +241,11 @@ def test_store_inferred_topics_data():
     shutil.rmtree(target_folder)
 
 
-def test_load_inferred_topics_data():
+@pytest.mark.parametrize("opts", list(TOPIC_MODELING_OPTS.values()))
+def test_load_inferred_topics_data(opts):
 
     # Arrange
-    inferred_model = compute_inferred_model()
+    inferred_model = compute_inferred_model(opts['method'], opts['run_opts'])
 
     test_inferred_topics_data: InferredTopicsData = topic_modelling.compile_inferred_topics_data(
         topic_model=inferred_model.topic_model,
