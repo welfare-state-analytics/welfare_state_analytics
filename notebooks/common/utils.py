045f422ad4e5c06c503598c3c9a683a9741b17cc
Roger Mähler <roger.mahler@hotmail.com>
2020-12-31 08:54:03 +0100

    Removed dead code

--- notebooks/common/utils.py
+++ notebooks/common/utils.py
@@ -14,3 +14,27 @@ def setup_pandas():
 
 def to_text(document: List[Tuple[int, int]], id2token: Dict[int, str]):
     return ' '.join(flatten([f * [id2token[token_id]] for token_id, f in document]))
+
+# from zipfile import ZipFile, ZipInfo
+
+# class ZipFileExt(ZipFile):
+
+#     class ZipFileIterator:
+
+#         def __init__(self, zipfile: ZipFile):
+#             self.zipfile = zipfile
+#             self.zipinfos: List[ZipInfo] = zipfile.infolist()
+#             self.zipiter: Iterator[ZipInfo] = iter(self.zipinfos)
+
+#         def __iter__(self):
+#             return self
+
+#         def __next__(self):
+#             next_info = next(self.zipiter)
+#             if next_info is None:
+#                 raise StopIteration
+#             return self._zipfile.open(next_info)
+
+#     def __iter__(self):
+#         return self.ZipFileIterator(self)
+
