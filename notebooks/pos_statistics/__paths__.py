19689cffe59ecf5fe71a7fdf167f150cf0434298
Roger Mähler <roger.mahler@hotmail.com>
2021-01-24 12:37:55 +0100

    Added PoS statistics notebook

---
+++ notebooks/pos_statistics/__paths__.py
@@ -0,0 +1,23 @@
+import os
+import sys
+
+
+def find_root_folder(x):
+    return os.path.join(os.getcwd().split(x)[0], x)
+
+
+root_folder = ''
+
+if os.environ.get("JUPYTER_IMAGE_SPEC", "") != "":
+    root_folder = "/home/jovyan/work/welfare_state_analytics"
+else:
+    root_folder = find_root_folder("welfare_state_analytics")
+
+if root_folder not in sys.path:
+    sys.path.insert(0, root_folder)
+
+ROOT_FOLDER = root_folder
+
+data_folder = os.path.join(root_folder, "data")
+work_folder = os.path.join(root_folder, "data")
+resources_folder = os.path.join(root_folder, "resources")
---
+++ notebooks/pos_statistics/pos_statistics.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.7.1
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# %% [markdown]
+# # Token Count Statistics
+# ### Text Processing Pipeline
+#
+# | | Building block | Arguments | Description |
+# | -- | :------------- | :------------- | :------------- |
+# | 💾 | <b>Checkpoint</b> | checkpoint_filename | Checkpoint (tagged frames) to file
+#
+# The PoS tagging notebook uses the same processing pipeline as the Word trends tnotebook  do to produce a tagged data frame. The processing will henceread
+# a checkpoint file if it exists, otherwise resolve the full pipeline.
+#
+# The word count statistics are collected in the tagging task (part-of-speech and lemma annotation). The computed statistics, total word count and the word counts for each PoS-grouping, are added (or updated) to the _document index file_ as new columns. This file is stored in the tagged text archive as `document_index.csv`.
+#
+# Note: The dcument index file is either a pre-existing document index or, if no such index exists, automatically generated during the initial text loading pipeline task.
+# If no pre-existing file exists, then the necessary attributes (e.g. document's year) are extracted from the filename of each  document.
+
+# %% tags=[]
+from IPython.core.display import display
+from penelope.notebook.token_counts import tokens_count_gui
+
+import __paths__  # pylint: disable=unused-import
+
+gui = tokens_count_gui.create_token_count_gui(
+    data_folder=__paths__.data_folder,
+    resources_folder=__paths__.resources_folder,
+)
+display(gui.layout())
+# %%
