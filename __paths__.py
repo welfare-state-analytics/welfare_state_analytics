82e498d32de64b851018b667d8bab5416170bc12
Roger Mähler <roger.mahler@hotmail.com>
2021-03-29 10:35:55 +0200

    Added data folder

--- __paths__.py
+++ __paths__.py
@@ -2,23 +2,23 @@ import os
 import sys
 
 
-def find_root_folder(x):
+def find_root_folder(x: str) -> str:
     return os.path.join(os.getcwd().split(x)[0], x)
 
 
-project_name = 'welfare_state_analytics'
-root_folder = ''
+project_name: str = 'welfare_state_analytics'
 
 if os.environ.get("JUPYTER_IMAGE_SPEC", "") != "":
-    root_folder = f"/home/jovyan/work/{project_name}"
+    root_folder: str = f"/home/jovyan/work/{project_name}"
+    corpus_folder: str = "/data/westac"
 else:
-    root_folder = find_root_folder(project_name)
+    root_folder: str = find_root_folder(project_name)
+    corpus_folder: str = os.path.join(root_folder, "data")
 
 if root_folder not in sys.path:
     sys.path.insert(0, root_folder)
 
-ROOT_FOLDER = root_folder
+ROOT_FOLDER: str = root_folder
 
-data_folder = os.path.join(root_folder, "data")
-output_folder = os.path.join(root_folder, "output")
-resources_folder = os.path.join(root_folder, "resources")
+data_folder: str = os.path.join(root_folder, "data")
+resources_folder: str = os.path.join(root_folder, "resources")
