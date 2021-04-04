c7bdd14c18f04b2c7456da20035025486a7453ad
Roger Mähler <roger.mahler@hotmail.com>
2021-04-04 13:17:07 +0200

    Renamed opts

--- tests/corpus_pipeline/pipeline_test.py
+++ tests/corpus_pipeline/pipeline_test.py
@@ -1,14 +1,14 @@
 from typing import List
 
 import pandas as pd
-from penelope.pipeline import ContentType, CorpusConfig, CorpusPipeline, CheckpointSerializeOpts, DocumentPayload
+from penelope.pipeline import ContentType, CorpusConfig, CorpusPipeline, CheckpointOpts, DocumentPayload
 
 
 def test_load_corpus_config_returns_correctly_escaped_values():
 
     config: CorpusConfig = CorpusConfig.load(path='./tests/test_data/riksdagens-protokoll.yml')
 
-    assert config.checkpoint_serialize_opts.sep == '\t'
+    assert config.checkpoint_opts.sep == '\t'
     assert config.text_reader_opts.filename_fields[0] == r'year:prot\_(\d{4}).*'
     assert config.text_reader_opts.filename_fields[1] == r'year2:prot_\d{4}(\d{2})__*'
     assert config.text_reader_opts.filename_fields[2] == r'number:prot_\d+[afk_]{0,4}__(\d+).*'
@@ -20,9 +20,9 @@ def test_pipeline_can_load_pos_tagged_checkpoint():
 
     corpus_filename: str = './tests/test_data/riksdagens-protokoll.1920-2019.test.sparv4.csv.zip'
     config: CorpusConfig = CorpusConfig.load(path='./tests/test_data/riksdagens-protokoll.yml')
-    options: CheckpointSerializeOpts = config.serialize_opts.as_type(ContentType.TAGGED_FRAME)
+    checkpoint_opts: CheckpointOpts = config.serialize_opts.as_type(ContentType.TAGGED_FRAME)
     pipeline = CorpusPipeline(config=config).load_tagged_frame(
-        corpus_filename, options, extra_reader_opts=config.text_reader_opts
+        corpus_filename, checkpoint_opts, extra_reader_opts=config.text_reader_opts
     )
 
     payloads: List[DocumentPayload] = pipeline.to_list()
