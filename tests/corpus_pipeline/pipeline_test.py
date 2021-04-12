10ef49535b5bb2a77cb8bd8ef85601408e895a74
Roger Mähler <roger.mahler@hotmail.com>
2021-04-12 15:56:05 +0200

    Renamed argument

--- tests/corpus_pipeline/pipeline_test.py
+++ tests/corpus_pipeline/pipeline_test.py
@@ -1,7 +1,7 @@
 from typing import List
 
 import pandas as pd
-from penelope.pipeline import ContentType, CorpusConfig, CorpusPipeline, CheckpointOpts, DocumentPayload
+from penelope.pipeline import CheckpointOpts, ContentType, CorpusConfig, CorpusPipeline, DocumentPayload
 
 
 def test_load_corpus_config_returns_correctly_escaped_values():
@@ -20,9 +20,9 @@ def test_pipeline_can_load_pos_tagged_checkpoint():
 
     corpus_filename: str = './tests/test_data/riksdagens-protokoll.1920-2019.test.sparv4.csv.zip'
     config: CorpusConfig = CorpusConfig.load(path='./tests/test_data/riksdagens-protokoll.yml')
-    checkpoint_opts: CheckpointOpts = config.serialize_opts.as_type(ContentType.TAGGED_FRAME)
+    checkpoint_opts: CheckpointOpts = config.checkpoint_opts.as_type(ContentType.TAGGED_FRAME)
     pipeline = CorpusPipeline(config=config).load_tagged_frame(
-        corpus_filename, checkpoint_opts, extra_reader_opts=config.text_reader_opts
+        corpus_filename, checkpoint_opts=checkpoint_opts, extra_reader_opts=config.text_reader_opts
     )
 
     payloads: List[DocumentPayload] = pipeline.to_list()
