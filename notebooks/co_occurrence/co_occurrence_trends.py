008fb88b089966648cfc02281e0e92480f0ac5c3
Roger Mähler <roger.mahler@hotmail.com>
2021-04-29 15:53:33 +0200

    Jupytext update

--- notebooks/co_occurrence/co_occurrence_trends.py
+++ notebooks/co_occurrence/co_occurrence_trends.py
@@ -7,7 +7,7 @@
 #       extension: .py
 #       format_name: percent
 #       format_version: '1.3'
-#       jupytext_version: 1.7.1
+#       jupytext_version: 1.11.1
 #   kernelspec:
 #     display_name: Python 3
 #     language: python
@@ -114,7 +114,7 @@ gui = main_gui.MainGUI(
     corpus_folder=__paths__.corpus_folder,
     data_folder=__paths__.data_folder,
     resources_folder=__paths__.resources_folder,
-    global_count_threshold=250,
+    global_count_threshold=1,
 )
 display(gui.layout())
 
