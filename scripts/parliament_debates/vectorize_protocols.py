d2e684e52a193a38dff65b6c353e4498b82ef3d7
Roger Mähler <roger.mahler@hotmail.com>
2021-05-19 03:58:58 +0200

    Renamed files

--- scripts/parliament_debates/vectorize_protocols.py
+++ scripts/parliament_debates/vectorize_protocols.py
@@ -5,7 +5,7 @@ import os
 import click
 import loguru
 import penelope.notebook.interface as interface
-import westac.parliamentary_debates.pipelines as pipe
+import westac.parliamentary_debates.pipelines as rp_pipeline
 from penelope.corpus import ExtractTaggedTokensOpts, TokensTransformOpts, VectorizeOpts
 from penelope.pipeline import CorpusConfig, CorpusPipeline  # ,  Token2Id
 
@@ -34,6 +34,13 @@ def profile_vectorize(ctx: click.Context):
     '--pos-includes',
     default=None,
     help='List of POS tags to include e.g. "|NOUN|JJ|".',
+    type=click.STRING,
+)
+@click.option(
+    '-i',
+    '--pos-paddings',
+    default=None,
+    help='List of POS tags to include in output as padded tokens.',
     type=click.STRING,
 )
 @click.option(
@@ -68,6 +75,57 @@ def profile_vectorize(ctx: click.Context):
     is_flag=True,
     help='Keep tokens with at least one alphanumeric char',
 )
+@click.option(
+    '--merge-speeches',
+    default=False,
+    is_flag=True,
+    help='Merge speeches in each protocol into a single document',
+)
+def _vectorize(
+    input_folder: str = None,
+    output_folder: str = None,
+    config: str = None,
+    output_tag: str = None,
+    create_subfolder: bool = True,
+    pos_includes: str = '|NN|',
+    pos_excludes: str = '|MAD|MID|PAD|',
+    pos_paddings: str = None,
+    to_lowercase: bool = True,
+    lemmatize: bool = True,
+    remove_stopwords: str = None,
+    min_word_length: int = 2,
+    max_word_length: int = None,
+    keep_symbols: bool = False,
+    keep_numerals: bool = False,
+    only_any_alphanumeric: bool = False,
+    only_alphabetic: bool = False,
+    count_threshold: int = None,
+    merge_speeches: bool = False,
+):
+
+    vectorize(
+        input_folder=input_folder,
+        output_folder=output_folder,
+        config=config,
+        output_tag=output_tag,
+        create_subfolder=create_subfolder,
+        pos_includes=pos_includes,
+        pos_excludes=pos_excludes,
+        pos_paddings=pos_paddings,
+        to_lowercase=to_lowercase,
+        lemmatize=lemmatize,
+        remove_stopwords=remove_stopwords,
+        min_word_length=min_word_length,
+        max_word_length=max_word_length,
+        keep_symbols=keep_symbols,
+        keep_numerals=keep_numerals,
+        only_any_alphanumeric=only_any_alphanumeric,
+        only_alphabetic=only_alphabetic,
+        count_threshold=count_threshold,
+        merge_speeches=merge_speeches,
+    )
+
+
 def vectorize(
     input_folder: str = None,
     output_folder: str = None,
@@ -87,6 +145,7 @@ def vectorize(
     only_any_alphanumeric: bool = False,
     only_alphabetic: bool = False,
     count_threshold: int = None,
+    merge_speeches: bool = False,
 ):
 
     try:
@@ -100,7 +159,7 @@ def vectorize(
             corpus_filename=input_folder,
             target_folder=output_folder,
             corpus_tag=output_tag,
-            tokens_transform_opts=TokensTransformOpts(
+            transform_opts=TokensTransformOpts(
                 to_lower=to_lowercase,
                 to_upper=False,
                 min_len=min_word_length,
@@ -116,13 +175,13 @@ def vectorize(
                 only_any_alphanumeric=only_any_alphanumeric,
             ),
             text_reader_opts=corpus_config.text_reader_opts,
-            extract_tagged_tokens_opts=ExtractTaggedTokensOpts(
+            extract_opts=ExtractTaggedTokensOpts(
                 pos_includes=pos_includes,
                 pos_excludes=pos_excludes,
                 pos_paddings=pos_paddings,
                 lemmatize=lemmatize,
             ),
-            tagged_tokens_filter_opts=None,
+            filter_opts=None,
             vectorize_opts=VectorizeOpts(already_tokenized=True),
             count_threshold=count_threshold,
             create_subfolder=create_subfolder,
@@ -130,19 +189,19 @@ def vectorize(
         )
         # parliament_data = ParliamentaryMembers.load()
         _: CorpusPipeline = (
-            pipe.to_tagged_frame_pipeline(
+            rp_pipeline.to_tagged_frame_pipeline(
                 source_folder=input_folder,
                 corpus_config=corpus_config,
                 checkpoint_filter=None,
                 filename_filter=None,
                 filename_pattern=None,
                 show_progress=True,
+                merge_speeches=merge_speeches,
             )
             .tagged_frame_to_tokens(
-                extract_opts=args.extract_tagged_tokens_opts,
-                filter_opts=args.tagged_tokens_filter_opts,
+                extract_opts=args.extract_opts,
+                filter_opts=args.filter_opts,
             )
-            .checkpoint('./tests/output/tagged_frame_to_tokens.zip')
             .exhaust(100)
         )
         # for payload in pipeline.take(1000): pass
@@ -155,4 +214,4 @@ def vectorize(
 
 
 if __name__ == '__main__':
-    vectorize()  # pylint: disable=no-value-for-parameter
+    _vectorize()  # pylint: disable=no-value-for-parameter
