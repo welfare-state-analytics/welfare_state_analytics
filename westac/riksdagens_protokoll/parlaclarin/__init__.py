9bca7e8e2afb8540b3e26fc831e943605298f732
Roger Mähler <roger.mahler@hotmail.com>
2021-11-22 10:45:22 +0100

    Added exports

--- westac/riksdagens_protokoll/parlaclarin/__init__.py
+++ westac/riksdagens_protokoll/parlaclarin/__init__.py
@@ -3,4 +3,4 @@
 from .tasks import ToTaggedFrame, ToIdTaggedFrame
 from .checkpoint import load_checkpoints, ParlaCsvContentSerializer
 from .members import ParliamentaryData
-from .pipelines import to_tagged_frame_pipeline
+from .pipelines import to_tagged_frame_pipeline, to_id_tagged_frame_pipeline, to_topic_model_pipeline
