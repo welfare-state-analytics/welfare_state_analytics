984856d92d01335c1aeb6aa40a241fc33f3e9c04
Roger Mähler <roger.mahler@hotmail.com>
2022-01-10 17:47:13 +0100

    Fixed test

--- notebooks/riksdagens_protokoll/token_counts/pos_statistics_gui.py
+++ notebooks/riksdagens_protokoll/token_counts/pos_statistics_gui.py
@@ -2,7 +2,9 @@ from typing import List, Union
 
 import pandas as pd
 from ipydatagrid import DataGrid
+from loguru import logger
 from penelope.notebook import token_counts as tc
+from penelope.notebook.token_counts.plot import plot_by_bokeh as plot_dataframe
 from westac.riksprot.parlaclarin import metadata
 
 # pylint: disable=unused-argument
@@ -12,7 +14,7 @@ class PoSCountGUI(tc.BasicDTMGUI):
     def __init__(self, *, default_folder: str, encoded: bool = True):
         super().__init__(
             default_folder=default_folder,
-            pivot_keys=['gender_id', 'party_abbrev_id', 'role_type_id'],
+            pivot_keys={'gender': 'gender_id', 'party_abbrev': 'party_abbrev_id', 'role_type': 'role_type_id'},
         )
         self.encoded: bool = encoded
         self.protocol_metadata: metadata.ProtoMetaData = None
@@ -30,9 +32,9 @@ class PoSCountGUI(tc.BasicDTMGUI):
         self.protocol_metadata = metadata.ProtoMetaData.load_from_same_folder(source)
         return self
 
-    def compute(self, df: pd.DataFrame, opts: tc.ComputeOpts) -> pd.DataFrame:
-        data: pd.DataFrame = super().compute(df, opts)
-        if self.encoded:
+    def compute(self) -> pd.DataFrame:
+        data: pd.DataFrame = super().compute()
+        if data is not None and self.encoded:
             data = self.protocol_metadata.decode_members_data(data)
         return data
 
