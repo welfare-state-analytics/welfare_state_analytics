c504724b7a6caecb0cafccaa23a6f33ff7380d86
Roger Mähler <roger.mahler@hotmail.com>
2022-01-20 16:01:28 +0100

    Silenced lint

--- __paths__.py
+++ __paths__.py
@@ -1,10 +1,10 @@
-from penelope.utility.paths import find_data_folder, find_root_folder, find_resources_folder
+from penelope.utility.paths import find_data_folder, find_resources_folder, find_root_folder
 
 project_name: str = 'welfare_state_analytics'
 project_short_name: str = "westac"
 
 corpus_folder: str = find_data_folder(project_name=project_name, project_short_name=project_short_name)
-root_folder: str = find_root_folder(project_name)
+root_folder: str = find_root_folder(project_name=project_name)
 resources_folder: str = find_resources_folder(project_name=project_name, project_short_name=project_short_name)
 
 data_folder: str = corpus_folder
