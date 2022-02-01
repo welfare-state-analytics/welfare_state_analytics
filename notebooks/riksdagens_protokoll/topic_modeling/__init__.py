71bd37cc9a0e3a7bc5fedb2f64a683ce80be3e99
Roger Mähler <roger.mahler@hotmail.com>
2022-02-01 21:38:43 +0100

    Added LoadGUI

--- notebooks/riksdagens_protokoll/topic_modeling/__init__.py
+++ notebooks/riksdagens_protokoll/topic_modeling/__init__.py
@@ -3,4 +3,4 @@
 from .topic_documents_gui import RiksprotBrowseTopicDocumentsGUI, RiksprotFindTopicDocumentsGUI
 from .topic_trends_gui import RiksprotTopicTrendsGUI, RiksprotTopicTrendsOverviewGUI
 from .topic_network_gui import RiksprotTopicTopicGUI
-from .pivot_network_gui import PivotTopicNetworkGUI
+from .load_gui import RiksprotLoadGUI
