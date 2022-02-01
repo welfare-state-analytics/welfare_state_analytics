ae6703684b1f703530ebe23406ede1361e3f1999
Roger Mähler <roger.mahler@hotmail.com>
2022-02-01 21:33:49 +0100

    Added load UI

---
+++ notebooks/riksdagens_protokoll/topic_modeling/load_gui.py
@@ -0,0 +1,26 @@
+from __future__ import annotations
+
+from penelope.notebook import topic_modelling as ntm
+from penelope import topic_modelling  as tm
+from penelope import pipeline as pp
+from westac.riksprot.parlaclarin import metadata as md
+
+
+class RiksprotLoadGUI(ntm.LoadGUI):
+    def __init__(
+        self,
+        riksprot_metadata: md.ProtoMetaData,
+        corpus_folder: str,
+        state: ntm.TopicModelContainer,
+        corpus_config: pp.CorpusConfig | None = None,
+        slim: bool = False,
+    ):
+        super().__init__(corpus_folder, state, corpus_config, slim)
+        self.riksprot_metadata: md.ProtoMetaData = riksprot_metadata
+
+    def load(self):
+        super().load()
+        inferred_topics: tm.InferredTopicsData = self.state["inferred_topics"]
+        inferred_topics.document_index = self.riksprot_metadata.overload_by_member_data(
+            inferred_topics.document_index, encoded=True, drop=True
+        )
