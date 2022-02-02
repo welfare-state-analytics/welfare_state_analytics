dfa94977fb292d081ba8ce684e7ffe1a6229bfe8
Roger Mähler <roger.mahler@hotmail.com>
2022-02-02 19:45:10 +0100

    Resolves #165

--- __paths__.py
+++ __paths__.py
@@ -1,3 +1,5 @@
+import sys
+
 from penelope.utility.paths import find_data_folder, find_resources_folder, find_root_folder
 
 project_name: str = 'welfare_state_analytics'
@@ -8,3 +10,6 @@ root_folder: str = find_root_folder(project_name=project_name)
 resources_folder: str = find_resources_folder(project_name=project_name, project_short_name=project_short_name)
 
 data_folder: str = corpus_folder
+
+if root_folder not in sys.path:
+    sys.path.insert(0, root_folder)
