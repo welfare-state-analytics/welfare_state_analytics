65dcfa880e9f67af3b2c8cad8f8600415a1c5f26
Roger Mähler <roger.mahler@hotmail.com>
2022-03-16 10:16:00 +0100

    Deprecated file

--- tests/riksdagens_protokoll/parlaclarin/members_test.py
+++ tests/riksdagens_protokoll/parlaclarin/members_test.py
@@ -1,111 +1,115 @@
-from io import StringIO
+# from io import StringIO
 
-import pandas as pd
-import pytest
+# import pandas as pd
+# import pytest
 
-from westac.riksprot.parlaclarin.metadata import ProtoMetaData
+# from westac.riksprot.parlaclarin import metadata as md
 
-from .fixtures import ID_COLUMNS, NAME_COLUMNS, SAMPLE_MEMBERS, sample_document_index
+# from .fixtures import ID_COLUMNS, NAME_COLUMNS, SAMPLE_MEMBERS, sample_document_index
 
-# pylint: disable=redefined-outer-name
-
-
-def sample_members(folder: str) -> None:
-    filename: str = 'tests/test_data/person_index.feather'
-    mps: pd.DataFrame = ProtoMetaData.load_members(folder)
-    df = pd.concat(
-        [
-            mps[mps.role_type == 'talman'].sample(n=2),
-            mps[mps.role_type == 'minister'].sample(n=2),
-            mps[mps.role_type == 'member'].sample(n=10),
-        ]
-    )
-    df.reset_index().to_feather(filename)
+# # pylint: disable=redefined-outer-name
 
 
-@pytest.fixture
-def members() -> pd.DataFrame:
-    filename: str = 'tests/test_data/person_index.feather'
-    return ProtoMetaData.read_members(filename)
+# def sample_members(folder: str) -> None:
+#     filename: str = 'tests/test_data/person_index.feather'
+#     mps: pd.DataFrame = md.IRiksprotMetaData.load_members(folder)
+#     df = pd.concat(
+#         [
+#             mps[mps.role_type == 'talman'].sample(n=2),
+#             mps[mps.role_type == 'minister'].sample(n=2),
+#             mps[mps.role_type == 'member'].sample(n=10),
+#         ]
+#     )
+#     df.reset_index().to_feather(filename)
 
 
-@pytest.fixture
-def document_index() -> pd.DataFrame:
-    return sample_document_index()
+# @pytest.fixture
+# def members() -> pd.DataFrame:
+#     filename: str = 'tests/test_data/person_index.feather'
+#     return md.IRiksprotMetaData.read_members(filename)
 
 
-def test_members_load():
-
-    mps: pd.DataFrame = ProtoMetaData.load_members(StringIO('\n'.join(SAMPLE_MEMBERS)))
-    assert isinstance(mps, pd.DataFrame)
-    assert len(mps) == 15
-
-    mps: pd.DataFrame = ProtoMetaData.load_members('tests/test_data/person_index.feather')
-    assert isinstance(mps, pd.DataFrame)
-    assert len(mps) == 15
-
-    mps: pd.DataFrame = ProtoMetaData.load_members('tests/test_data')
-    assert isinstance(mps, pd.DataFrame)
-    assert len(mps) == 15
+# @pytest.fixture
+# def document_index() -> pd.DataFrame:
+#     return sample_document_index()
 
 
-def test_create_metadata(members):
+# def test_members_load():
 
-    md: ProtoMetaData = ProtoMetaData(members=members)
+#     mps: pd.DataFrame = md.IRiksprotMetaData.load_members(StringIO('\n'.join(SAMPLE_MEMBERS)))
+#     assert isinstance(mps, pd.DataFrame)
+#     assert len(mps) == 15
 
-    assert md is not None
-    assert len(md.members) == 15
-    assert len(md.role_type2id) == 4 == len(md.role_type2name)
-    assert len(md.gender2id) == 3 == len(md.gender2name) == len(md.genders)
-    assert len(md.party_abbrevs) == 5
+#     mps: pd.DataFrame = md.IRiksprotMetaData.load_members('tests/test_data/person_index.feather')
+#     assert isinstance(mps, pd.DataFrame)
+#     assert len(mps) == 15
 
-    assert not md.members.party_abbrev.isna().any()
-    assert not md.members.gender.isna().any()
-    assert not md.members.role_type.isna().any()
-    assert not md.party_abbrevs.party_abbrev.isna().any()
-    assert not md.genders.gender.isna().any()
-
-    assert md.who2id['hans_blix_minister_1978'] == 3
-    assert md.id2who[3] == 'hans_blix_minister_1978'
-
-    assert md.gender2id['woman'] == 2
-    assert md.gender2name[2] == 'woman'
-
-    assert md.role_type2id['talman'] == 1
-    assert md.role_type2name[1] == 'talman'
+#     mps: pd.DataFrame = md.IRiksprotMetaData.load_members('tests/test_data')
+#     assert isinstance(mps, pd.DataFrame)
+#     assert len(mps) == 15
 
 
-def test_encoded_members(members):
+# def test_create_metadata(members):
 
-    md: ProtoMetaData = ProtoMetaData(members=members)
+#     riksprot_metadata: md.IRiksprotMetaData = md.IRiksprotMetaData.load(members=members)
 
-    em: pd.DataFrame = md.encoded_members
+#     assert riksprot_metadata is not None
+#     assert len(riksprot_metadata.members) == 15
+#     assert len(riksprot_metadata.role_type2id) == 4 == len(riksprot_metadata.role_type2name)
+#     assert len(riksprot_metadata.gender2id) == 3 == len(riksprot_metadata.gender2name) == len(riksprot_metadata.genders)
+#     assert len(riksprot_metadata.party_abbrevs) == 5
 
-    assert em is not None
+#     assert not riksprot_metadata.members.party_abbrev.isna().any()
+#     assert not riksprot_metadata.members.gender.isna().any()
+#     assert not riksprot_metadata.members.role_type.isna().any()
+#     assert not riksprot_metadata.party_abbrevs.party_abbrev.isna().any()
+#     assert not riksprot_metadata.genders.gender.isna().any()
 
-    assert len(em) == len(md.members)
-    assert set(ID_COLUMNS).intersection(set(em.columns)) == set(ID_COLUMNS)
-    assert set(NAME_COLUMNS).intersection(set(em.columns)) == set()
+#     assert riksprot_metadata.who2id['hans_blix_minister_1978'] == 3
+#     assert riksprot_metadata.id2who[3] == 'hans_blix_minister_1978'
 
-    em_back = md.decode_members_data(em, drop=True)
+#     assert riksprot_metadata.gender2id['woman'] == 2
+#     assert riksprot_metadata.gender2name[2] == 'woman'
 
-    assert len(em_back) == len(md.members)
-    assert set(ID_COLUMNS).intersection(set(em_back.columns)) == set()
-    assert set(NAME_COLUMNS).intersection(set(em_back.columns)) == set(NAME_COLUMNS)
+#     assert riksprot_metadata.role_type2id['talman'] == 1
+#     assert riksprot_metadata.role_type2name[1] == 'talman'
 
 
-def test_overload_by_member_data(members, document_index):
+# def test_encoded_members(members):
 
-    md: ProtoMetaData = ProtoMetaData(members=members)
+#     riksprot_metadata: md.IRiksprotMetaData = md.IRiksprotMetaData.load(members=members)
 
-    dx: pd.DataFrame = md.overload_by_member_data(document_index)
+#     em: pd.DataFrame = riksprot_metadata.encoded_members
 
-    assert dx is not None
+#     assert em is not None
 
-    assert set(ID_COLUMNS).intersection(set(dx.columns)) == set(ID_COLUMNS)
+#     assert len(em) == len(riksprot_metadata.members)
+#     assert set(ID_COLUMNS).intersection(set(em.columns)) == set(ID_COLUMNS)
+#     assert set(NAME_COLUMNS).intersection(set(em.columns)) == set()
 
-    dx: pd.DataFrame = md.overload_by_member_data(document_index, encoded=True, drop=True, columns='gender_id')
-    assert set(ID_COLUMNS).intersection(set(dx.columns)) == set(['gender_id'])
+#     em_back = riksprot_metadata.decode_members_data(em, drop=True)
 
-    dx: pd.DataFrame = md.overload_by_member_data(document_index, encoded=False, drop=True, columns='gender')
-    assert set(NAME_COLUMNS).intersection(set(dx.columns)) == set(['gender'])
+#     assert len(em_back) == len(riksprot_metadata.members)
+#     assert set(ID_COLUMNS).intersection(set(em_back.columns)) == set()
+#     assert set(NAME_COLUMNS).intersection(set(em_back.columns)) == set(NAME_COLUMNS)
+
+
+# def test_overload_by_member_data(members, document_index):
+
+#     riksprot_metadata: md.IRiksprotMetaData = md.IRiksprotMetaData.load(members=members)
+
+#     dx: pd.DataFrame = riksprot_metadata.overload_by_member_data(document_index)
+
+#     assert dx is not None
+
+#     assert set(ID_COLUMNS).intersection(set(dx.columns)) == set(ID_COLUMNS)
+
+#     dx: pd.DataFrame = riksprot_metadata.overload_by_member_data(
+#         document_index, encoded=True, drop=True, columns='gender_id'
+#     )
+#     assert set(ID_COLUMNS).intersection(set(dx.columns)) == set(['gender_id'])
+
+#     dx: pd.DataFrame = riksprot_metadata.overload_by_member_data(
+#         document_index, encoded=False, drop=True, columns='gender'
+#     )
+#     assert set(NAME_COLUMNS).intersection(set(dx.columns)) == set(['gender'])
