07f9e8637233ac78d146a833b0218e86f4761004
Roger Mähler <roger.mahler@hotmail.com>
2022-03-22 21:21:23 +0100

    Fixed path

--- tests/riksdagens_protokoll/kb_labb/pipeline_test.py
+++ tests/riksdagens_protokoll/kb_labb/pipeline_test.py
@@ -20,9 +20,7 @@ def test_load_corpus_config_returns_correctly_escaped_values():
 
 def test_pipeline_can_load_pos_tagged_checkpoint():
 
-    corpus_source: str = (
-        './tests/test_data/riksprot/kb_labb/riksdagens-protokoll.1920-2019.9files.sparv4.csv.zip'
-    )
+    corpus_source: str = './tests/test_data/riksprot/kb_labb/riksdagens-protokoll.1920-2019.9files.sparv4.csv.zip'
     config: CorpusConfig = CorpusConfig.load(CONFIG_FILENAME)
     checkpoint_opts: CheckpointOpts = config.checkpoint_opts.as_type(ContentType.TAGGED_FRAME)
     pipeline = CorpusPipeline(config=config).load_tagged_frame(
