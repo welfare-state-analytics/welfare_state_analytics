7aa08e64ada44ac8eef6a953e1f09073a4c14468
Roger Mähler <roger.mahler@hotmail.com>
2022-03-22 08:29:10 +0100

    Switch to v0.4.1

--- westac/riksprot/parlaclarin/__init__.py
+++ westac/riksprot/parlaclarin/__init__.py
@@ -1,9 +1,6 @@
 from .speech_text import SpeechTextRepository
-from .metadata import (
-    MemberNotFoundError,
-    ProtoMetaData,
-    ROLE_TYPE2ID,
-    GENDER2ID,
-    MEMBER_IDNAME2NAME_MAPPING,
-    MEMBER_NAME2IDNAME_MAPPING,
+from .codecs import (
+    Codecs,
+    PersonCodecs,
+    CODE_TABLENAMES
 )
